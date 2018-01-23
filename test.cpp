477777adfadd8efce67f32940633a3a43e225734
Nick Maher <nick.maher@gmail.com>
2018-01-23 21:09:06 +1100

    Fixes #2
    Added newlines.

--- test.cpp
+++ test.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!";
-
-    // Fix for issue 1
-    std::cout << "This is a fix";
-
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "This is a fix" << std::endl;
     return 0;
 }
